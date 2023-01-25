28891d826827b48564417bac94a01412428898dc
Miquelane Melo <miquelanepmelo@gmail.com>
2023-01-25 14:25:33 -0300

    class exceptions

---
+++ src/main/java/com/udemy/api/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.udemy.api.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String message){
+        super(message);
+    }
+
+
+}
