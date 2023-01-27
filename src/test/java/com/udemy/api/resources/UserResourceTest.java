c52233c24d99f09809bd437a989843b3892f794a
Miquelane Melo <miquelanepmelo@gmail.com>
2023-01-27 14:48:43 -0300

    class test UserResource create

--- src/test/java/com/udemy/api/resources/UserResourceTest.java
+++ src/test/java/com/udemy/api/resources/UserResourceTest.java
@@ -86,7 +86,13 @@ class UserResourceTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnCreated() {
+        when(service.create(any())).thenReturn(user);
+
+        ResponseEntity<UserDto> response = resource.create(userDto);
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getHeaders().get("Location"));
     }
 
     @Test
